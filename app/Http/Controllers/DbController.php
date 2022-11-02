2906f9efd7ddf462b903dbd7ec2401317269e974
Hackashi <nexagray@gmail.com>
2022-11-02 12:22:03 +0100

    optimize controller variables

--- app/Http/Controllers/DbController.php
+++ app/Http/Controllers/DbController.php
@@ -13,9 +13,9 @@ class DbController extends Controller
         $total = Membre::count();
         $hommes = Membre::all()->where('genre','Homme');
         $femmes = Membre::all()->where('genre','Femme');
-        $hommes18 = Membre::all()->where('genre', 'Homme')->whereBetween('age', [18,24] );
-        $femmes18 = Membre::all()->where('genre', 'Femme')->whereBetween('age', [18,24] );
-        $hc = Membre::all()->whereNotBetween('age',[18,24]);
+        $hommes18 = $hommes->whereBetween('age', [18,24] );
+        $femmes18 = $femmes->whereBetween('age', [18,24] );
+        $hc = $membres->whereNotBetween('age',[18,24]);
     return view ('home', compact('membres', 'hommes', 'femmes', 'total', 'hommes18', 'femmes18', 'hc'));
 
     }
